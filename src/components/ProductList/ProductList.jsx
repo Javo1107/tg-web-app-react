da13c4ca518abe4877f26ebeb4f461abe075dcb9
joe <jawoheer@gmail.com>
2024-08-20 16:39:58 +0500

    adfa

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://85.119.146.179:8000/web-data', {
+        fetch('http://3.25.146.155:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
