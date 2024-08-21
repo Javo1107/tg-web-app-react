d4312e5322378ea39b7102c0e1ef742c437cb769
joe <jawoheer@gmail.com>
2024-08-21 16:33:08 +0500

    sdf

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -38,8 +38,9 @@ const ProductList = () => {
         };
     
         try {
-            tg.showAlert("In try")
+            
             const response = await axios.post('http://3.25.146.155:8000/web-data', data, {
+
                 headers: {
                     'Content-Type': 'application/json',
                 },
@@ -48,7 +49,7 @@ const ProductList = () => {
             tg.showAlert("Success");
             console.log('Success:', response.data);
         } catch (error) {
-            //tg.showAlert(`Error: ${error.message}`);
+            tg.showAlert(`Error: ${error.message}`);
             console.error('Error:', error);
         }
     }, [addedItems, queryId]);
